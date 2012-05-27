bc2e8e59dbfa826003ce98b2323ff3e854b122fb
Titouan Van Belle <titouan.vanbelle@orange.fr>
2012-05-27 11:45:50 +0200

    Fixed bug : case sensitivity image names

--- Ext.ux.ThreeSixtyView/app/view/Main.js
+++ Ext.ux.ThreeSixtyView/app/view/Main.js
@@ -7,158 +7,152 @@ Ext.define("ThreeSixtyView.view.Main", {
 
   config: {
     fullscreen: true,
-
-    defaults: {
-      style: 'background: #fff',
-      xtype: 'container'
-    },
-
     items: [{
       xtype:'threesixtyview',
       data:[{ 
-        src : 'resources/images/Seq_v04_640x378_01.jpg'
+        src : 'resources/images/threesixty/Seq_v04_640x378_01.jpg'
       },  { 
-        src : 'resources/images/Seq_v04_640x378_02.jpg'
+        src : 'resources/images/threesixty/Seq_v04_640x378_02.jpg'
       },  { 
-        src : 'resources/images/Seq_v04_640x378_03.jpg'
+        src : 'resources/images/threesixty/Seq_v04_640x378_03.jpg'
       },  { 
-        src : 'resources/images/Seq_v04_640x378_04.jpg'
+        src : 'resources/images/threesixty/Seq_v04_640x378_04.jpg'
       },  { 
-        src : 'resources/images/Seq_v04_640x378_05.jpg'
+        src : 'resources/images/threesixty/Seq_v04_640x378_05.jpg'
       },  { 
-        src : 'resources/images/Seq_v04_640x378_06.jpg'
+        src : 'resources/images/threesixty/Seq_v04_640x378_06.jpg'
       },  { 
-        src : 'resources/images/Seq_v04_640x378_07.jpg'
+        src : 'resources/images/threesixty/Seq_v04_640x378_07.jpg'
       },  { 
-        src : 'resources/images/Seq_v04_640x378_08.jpg'
+        src : 'resources/images/threesixty/Seq_v04_640x378_08.jpg'
       },  { 
-        src : 'resources/images/Seq_v04_640x378_09.jpg'
+        src : 'resources/images/threesixty/Seq_v04_640x378_09.jpg'
       },  { 
-        src : 'resources/images/Seq_v04_640x378_10.jpg'
+        src : 'resources/images/threesixty/Seq_v04_640x378_10.jpg'
       }  ,  { 
-        src : 'resources/images/Seq_v04_640x378_11.jpg'
+        src : 'resources/images/threesixty/Seq_v04_640x378_11.jpg'
       }  ,  { 
-        src : 'resources/images/Seq_v04_640x378_12.jpg'
+        src : 'resources/images/threesixty/Seq_v04_640x378_12.jpg'
       }  ,  { 
-        src : 'resources/images/Seq_v04_640x378_13.jpg'
+        src : 'resources/images/threesixty/Seq_v04_640x378_13.jpg'
       }  ,  { 
-        src : 'resources/images/Seq_v04_640x378_14.jpg'
+        src : 'resources/images/threesixty/Seq_v04_640x378_14.jpg'
       }  ,  { 
-        src : 'resources/images/Seq_v04_640x378_15.jpg'
+        src : 'resources/images/threesixty/Seq_v04_640x378_15.jpg'
       }  ,  { 
-        src : 'resources/images/Seq_v04_640x378_16.jpg'
+        src : 'resources/images/threesixty/Seq_v04_640x378_16.jpg'
       }  ,  { 
-        src : 'resources/images/Seq_v04_640x378_17.jpg'
+        src : 'resources/images/threesixty/Seq_v04_640x378_17.jpg'
       }  ,  { 
-        src : 'resources/images/Seq_v04_640x378_18.jpg'
+        src : 'resources/images/threesixty/Seq_v04_640x378_18.jpg'
       }  ,  { 
-        src : 'resources/images/Seq_v04_640x378_19.jpg'
+        src : 'resources/images/threesixty/Seq_v04_640x378_19.jpg'
       }  ,  { 
-        src : 'resources/images/Seq_v04_640x378_20.jpg'
+        src : 'resources/images/threesixty/Seq_v04_640x378_20.jpg'
       }  ,  { 
-        src : 'resources/images/Seq_v04_640x378_21.jpg'
+        src : 'resources/images/threesixty/Seq_v04_640x378_21.jpg'
       }  ,  { 
-        src : 'resources/images/Seq_v04_640x378_22.jpg'
+        src : 'resources/images/threesixty/Seq_v04_640x378_22.jpg'
       }  ,  { 
-        src : 'resources/images/Seq_v04_640x378_23.jpg'
+        src : 'resources/images/threesixty/Seq_v04_640x378_23.jpg'
       }  ,  { 
-        src : 'resources/images/Seq_v04_640x378_24.jpg'
+        src : 'resources/images/threesixty/Seq_v04_640x378_24.jpg'
       }  ,  { 
-        src : 'resources/images/Seq_v04_640x378_25.jpg'
+        src : 'resources/images/threesixty/Seq_v04_640x378_25.jpg'
       }  ,  { 
-        src : 'resources/images/Seq_v04_640x378_26.jpg'
+        src : 'resources/images/threesixty/Seq_v04_640x378_26.jpg'
       }  ,  { 
-        src : 'resources/images/Seq_v04_640x378_27.jpg'
+        src : 'resources/images/threesixty/Seq_v04_640x378_27.jpg'
       }  ,  { 
-        src : 'resources/images/Seq_v04_640x378_28.jpg'
+        src : 'resources/images/threesixty/Seq_v04_640x378_28.jpg'
       }  ,  { 
-        src : 'resources/images/Seq_v04_640x378_29.jpg'
+        src : 'resources/images/threesixty/Seq_v04_640x378_29.jpg'
       }  ,  { 
-        src : 'resources/images/Seq_v04_640x378_30.jpg'
+        src : 'resources/images/threesixty/Seq_v04_640x378_30.jpg'
       }  ,  { 
-        src : 'resources/images/Seq_v04_640x378_31.jpg'
+        src : 'resources/images/threesixty/Seq_v04_640x378_31.jpg'
       }  ,  { 
-        src : 'resources/images/Seq_v04_640x378_32.jpg'
+        src : 'resources/images/threesixty/Seq_v04_640x378_32.jpg'
       }  ,  { 
-        src : 'resources/images/Seq_v04_640x378_33.jpg'
+        src : 'resources/images/threesixty/Seq_v04_640x378_33.jpg'
       }  ,  { 
-        src : 'resources/images/Seq_v04_640x378_34.jpg'
+        src : 'resources/images/threesixty/Seq_v04_640x378_34.jpg'
       }  ,  { 
-        src : 'resources/images/Seq_v04_640x378_35.jpg'
+        src : 'resources/images/threesixty/Seq_v04_640x378_35.jpg'
       }  ,  { 
-        src : 'resources/images/Seq_v04_640x378_36.jpg'
+        src : 'resources/images/threesixty/Seq_v04_640x378_36.jpg'
       }  ,  { 
-        src : 'resources/images/Seq_v04_640x378_37.jpg'
+        src : 'resources/images/threesixty/Seq_v04_640x378_37.jpg'
       }  ,  { 
-        src : 'resources/images/Seq_v04_640x378_38.jpg'
+        src : 'resources/images/threesixty/Seq_v04_640x378_38.jpg'
       }  ,  { 
-        src : 'resources/images/Seq_v04_640x378_39.jpg'
+        src : 'resources/images/threesixty/Seq_v04_640x378_39.jpg'
       }  ,  { 
-        src : 'resources/images/Seq_v04_640x378_40.jpg'
+        src : 'resources/images/threesixty/Seq_v04_640x378_40.jpg'
       }  ,  { 
-        src : 'resources/images/Seq_v04_640x378_41.jpg'
+        src : 'resources/images/threesixty/Seq_v04_640x378_41.jpg'
       }  ,  { 
-        src : 'resources/images/Seq_v04_640x378_42.jpg'
+        src : 'resources/images/threesixty/Seq_v04_640x378_42.jpg'
       }  ,  { 
-        src : 'resources/images/Seq_v04_640x378_43.jpg'
+        src : 'resources/images/threesixty/Seq_v04_640x378_43.jpg'
       }  ,  { 
-        src : 'resources/images/Seq_v04_640x378_44.jpg'
+        src : 'resources/images/threesixty/Seq_v04_640x378_44.jpg'
       }  ,  { 
-        src : 'resources/images/Seq_v04_640x378_45.jpg'
+        src : 'resources/images/threesixty/Seq_v04_640x378_45.jpg'
       }  ,  { 
-        src : 'resources/images/Seq_v04_640x378_46.jpg'
+        src : 'resources/images/threesixty/Seq_v04_640x378_46.jpg'
       }  ,  { 
-        src : 'resources/images/Seq_v04_640x378_47.jpg'
+        src : 'resources/images/threesixty/Seq_v04_640x378_47.jpg'
       }  ,  { 
-        src : 'resources/images/Seq_v04_640x378_48.jpg'
+        src : 'resources/images/threesixty/Seq_v04_640x378_48.jpg'
       }  ,  { 
-        src : 'resources/images/Seq_v04_640x378_49.jpg'
+        src : 'resources/images/threesixty/Seq_v04_640x378_49.jpg'
       }  ,  { 
-        src : 'resources/images/Seq_v04_640x378_50.jpg'
+        src : 'resources/images/threesixty/Seq_v04_640x378_50.jpg'
       }  ,  { 
-        src : 'resources/images/Seq_v04_640x378_51.jpg'
+        src : 'resources/images/threesixty/Seq_v04_640x378_51.jpg'
       }  ,  { 
-        src : 'resources/images/Seq_v04_640x378_52.jpg'
+        src : 'resources/images/threesixty/Seq_v04_640x378_52.jpg'
       }  ,  { 
-        src : 'resources/images/Seq_v04_640x378_53.jpg'
+        src : 'resources/images/threesixty/Seq_v04_640x378_53.jpg'
       }  ,  { 
-        src : 'resources/images/Seq_v04_640x378_54.jpg'
+        src : 'resources/images/threesixty/Seq_v04_640x378_54.jpg'
       }  ,  { 
-        src : 'resources/images/Seq_v04_640x378_55.jpg'
+        src : 'resources/images/threesixty/Seq_v04_640x378_55.jpg'
       }  ,  { 
-        src : 'resources/images/Seq_v04_640x378_56.jpg'
+        src : 'resources/images/threesixty/Seq_v04_640x378_56.jpg'
       }  ,  { 
-        src : 'resources/images/Seq_v04_640x378_57.jpg'
+        src : 'resources/images/threesixty/Seq_v04_640x378_57.jpg'
       }  ,  { 
-        src : 'resources/images/Seq_v04_640x378_58.jpg'
+        src : 'resources/images/threesixty/Seq_v04_640x378_58.jpg'
       }  ,  { 
-        src : 'resources/images/Seq_v04_640x378_59.jpg'
+        src : 'resources/images/threesixty/Seq_v04_640x378_59.jpg'
       }  ,  { 
-        src : 'resources/images/Seq_v04_640x378_60.jpg'
+        src : 'resources/images/threesixty/Seq_v04_640x378_60.jpg'
       }  ,  { 
-        src : 'resources/images/Seq_v04_640x378_61.jpg'
+        src : 'resources/images/threesixty/Seq_v04_640x378_61.jpg'
       }  ,  { 
-        src : 'resources/images/Seq_v04_640x378_62.jpg'
+        src : 'resources/images/threesixty/Seq_v04_640x378_62.jpg'
       }  ,  { 
-        src : 'resources/images/Seq_v04_640x378_63.jpg'
+        src : 'resources/images/threesixty/Seq_v04_640x378_63.jpg'
       }  ,  { 
-        src : 'resources/images/Seq_v04_640x378_64.jpg'
+        src : 'resources/images/threesixty/Seq_v04_640x378_64.jpg'
       }  ,  { 
-        src : 'resources/images/Seq_v04_640x378_65.jpg'
+        src : 'resources/images/threesixty/Seq_v04_640x378_65.jpg'
       }  ,  { 
-        src : 'resources/images/Seq_v04_640x378_66.jpg'
+        src : 'resources/images/threesixty/Seq_v04_640x378_66.jpg'
       }  ,  { 
-        src : 'resources/images/Seq_v04_640x378_67.jpg'
+        src : 'resources/images/threesixty/Seq_v04_640x378_67.jpg'
       }  ,  { 
-        src : 'resources/images/Seq_v04_640x378_68.jpg'
+        src : 'resources/images/threesixty/Seq_v04_640x378_68.jpg'
       }  ,  { 
-        src : 'resources/images/Seq_v04_640x378_69.jpg'
+        src : 'resources/images/threesixty/Seq_v04_640x378_69.jpg'
       }  ,  { 
-        src : 'resources/images/Seq_v04_640x378_70.jpg'
+        src : 'resources/images/threesixty/Seq_v04_640x378_70.jpg'
       }  ,  { 
-        src : 'resources/images/Seq_v04_640x378_71.jpg'
+        src : 'resources/images/threesixty/Seq_v04_640x378_71.jpg'
       }  ,  { 
-        src : 'resources/images/Seq_v04_640x378_72.jpg'
+        src : 'resources/images/threesixty/Seq_v04_640x378_72.jpg'
       }]
     }]
   }
