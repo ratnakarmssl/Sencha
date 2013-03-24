0ed0383ca3aa8947dafaafbe06c77b6de5dcd1b7
TitouanVanBelle <titouan.vanbelle@orange.fr>
2013-03-24 17:30:33 +0100

    Upgrade 2.1.1

---
+++ app.js
@@ -0,0 +1,92 @@
+Ext.Loader.setPath({
+  'Ext': 'touch/src',
+  'Ext.ux':   'app/ux'
+});
+
+Ext.application({
+  name: 'KitchenSink',
+
+  requires: [],
+
+  controllers: [
+    'Main',
+    'ViewB'
+  ],
+
+  views: [
+    'Main',
+    'ViewA',
+    'ViewB',
+    'ViewC'
+  ],
+
+  stores: [
+    'Tasks'
+  ],
+
+  models: [
+    'Task'
+  ],
+
+  icon: {
+    57: 'resources/icons/Icon.png',
+    72: 'resources/icons/Icon~ipad.png',
+    114: 'resources/icons/Icon@2x.png',
+    144: 'resources/icons/Icon~ipad@2x.png'
+  },
+
+  viewport: {
+    autoMaximize: true
+  },
+
+  phoneStartupScreen: 'resources/loading/Homescreen.jpg',
+  tabletStartupScreen: 'resources/loading/Homescreen~ipad.jpg',
+
+  launch: function() {
+    Ext.fly('appLoadingIndicator').destroy();
+
+    var store = Ext.getStore('Tasks');
+
+    store.removeAll();
+    store.sync();
+
+    if (store.getCount() <= 0) {
+      store.add([
+        {title: 'Apples', status: '2do'},
+        {title: 'Cheerios', status: '2do'},
+        {title: 'Milk', status: '2do'},
+        {title: 'Beer', status: '2do'}
+      ]);
+      store.sync();
+    }
+
+    store.load();
+
+    Ext.create('KitchenSink.view.Main').push({
+      title: 'Kitchen Sink',
+      xtype: 'list',
+      name: 'mainlist',
+      itemTpl: '{title}',
+      data: [{
+        title: 'Infinite Carousel',
+        xtype: 'viewa'
+      }, {
+        title: 'List Options',
+        xtype: 'viewb'
+      }, {
+        title: 'Reminder',
+        xtype: 'viewc'
+      }]
+    });
+  },
+
+  onUpdated: function() {
+    Ext.Msg.confirm(
+      "Application Update",
+      "This application has just successfully been updated to the latest version. Reload now?",
+      function() {
+        window.location.reload();
+      }
+    );
+  }
+});
